4fcf8a3d6da6bc3a570f433e8a97ac6870d4695c
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-15 12:13:10 +0100

    Modified name.rb with name repitition and name concatentation

--- name.rb
+++ name.rb
@@ -1,3 +1,13 @@
-print "What is your name? "
+print "What is your first name? "
 name = gets.chomp
 puts "Hello #{name}, how are you today?"
+
+print "What is your first name? "
+first_name = gets.chomp
+print "What is your last name? "
+last_name = gets.chomp
+puts first_name + " " + last_name 
+
+10.times do
+    puts name
+end
