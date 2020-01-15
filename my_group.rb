3fdabee92767e118ec82dd4ec7f5501be4130fe5
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-15 12:04:07 +0100

    Created name.rb and age.rb files for ruby exercises

--- my_group.rb
+++ my_group.rb
@@ -1,1 +1,11 @@
-my_group= [{:name=>"Kayla", :gender=>"Female", :age=>27}, {:name=>"Picard", :gender=>"Male", :age=>50}, {:name=>"Donna", :gender=>"Female", :age=>33}]
+my_group = []
+person_1 = {name:"Kayla" , gender: "Woman", age: 27}
+person_2 = {name:"Picard", gender: "Man", age:50}
+person_3 = {name:"Donna", gender: "Woman", age: 33}
+my_group.push person_1
+my_group.push person_2
+my_group.push person_3
+my_group.each do |my_group|  
+       
+puts "#{my_group[:name]} is a #{my_group[:age]} year old #{my_group[:gender].downcase!}"
+end
---
+++ name.rb
@@ -0,0 +1,3 @@
+print "What is your name? "
+name = gets.chomp
+puts "Hello #{name}, how are you today?"
